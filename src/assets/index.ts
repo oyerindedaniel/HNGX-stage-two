bbeb099083a8f7e82b8b35f32c4bc7b3f1cc6dcf
oyerindedaniel <oyerinde.daniel@yahoo.com>
2023-09-14 19:52:55 +0100

    deployment bug fix

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,24 +1,24 @@
-import HeaderImg from './Header.svg';
+import HeaderImg from './header.svg';
 import RottenTomatoes from './rotten-tomatoes.svg';
 import Imdb from './imdb.svg';
-import Calender from './Calendar.svg';
+import Calender from './calendar.svg';
 import Facebook from './facebook.svg';
 import Favourite from './favorite.svg';
-import Home from './Home.svg';
+import Home from './home.svg';
 import Instagram from './instagram.svg';
-import List from './List.svg';
+import List from './list.svg';
 import Logo from './logo.svg';
 import Logout from './logout.svg';
 import MovieProjector from './movie-projector.svg';
 import Play from './play.svg';
 import PlayIcon from './play-icon.svg';
-import Search from './Search.svg';
+import Search from './search.svg';
 import Star from './star.svg';
 import TvShow from './tv-show.svg';
 import Twitter from './twitter.svg';
 import TwoTickets from './two-tickets.svg';
 import Youtube from './youtube.svg';
-import Menu from './Menu.svg';
+import Menu from './menu.svg';
 
 export {
   HeaderImg,
